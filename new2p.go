6f728868f34dec284997df04a61472c22099c27f
the-duckie-2 <dakshshettar2023@gmail.com>
2024-08-31 04:24:00 +0530

    hm

---
+++ new2p.go
@@ -0,0 +1,9 @@
+package newp
+
+import (
+	"fmt"
+)
+
+func ByeWorld(s string) {
+	fmt.Printf("bye %s", s)
+}
---
+++ newp.go
@@ -0,0 +1,9 @@
+package newp
+
+import (
+	"fmt"
+)
+
+func HelloWorld(s string) {
+	fmt.Printf("hello %s", s)
+}
